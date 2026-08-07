2424c2f1fc1eb8137d50cfaab5e5e9ed9764047a
Invodor <ishaanbijith3@gmail.com>
2026-08-07 21:11:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Welcome to my portfolio!");
+
+document.querySelectorAll("nav a").forEach(link=>{
+    link.addEventListener("click",function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({behavior:"smooth"});
+    });
+});
